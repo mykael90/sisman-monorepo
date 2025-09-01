48f624979a3efa01510ac82c7ba8a6080f157f49
Mykael Mello <eng.mykael@gmail.com>
2025-09-01 08:43:59 -0300

    refactor: retirando o uso do hook isDesktop para melhor experiencia do usuario

--- apps/frontend/src/app/(main)/material/(warehouse)/withdrawal/_components/add/tab-selector.tsx
+++ apps/frontend/src/app/(main)/material/(warehouse)/withdrawal/_components/add/tab-selector.tsx
@@ -5,14 +5,13 @@ import {
   MaterialOperationOutKey,
   materialOperationOutDisplayMap as op
 } from '@/mappers/material-operations-mappers';
-import { useMediaQuery } from '@/hooks/use-media-query';
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue
-} from '@radix-ui/react-select';
+} from '@/components/ui/select';
 
 interface TabSelectorProps {
   movementTypeCode: MaterialOperationOutKey;
@@ -27,7 +26,11 @@ export function TabSelector({
   setMovementTypeCode,
   handleReset
 }: TabSelectorProps) {
-  const isDesktop = useMediaQuery('(min-width: 768px)');
+  //evitar o uso desse hook para não ter o efeito de 'blink'
+  // const isDesktop = useMediaQuery('(min-width: 768px)');
+
+  //poderia pensar no uso de uma função dinâmica no server para pegar o agente do headers, mas nesse caso teriamos
+  // um problema relacionado a performance, já que a página não poderia ser estática (usa uma informação da requisicao)
 
   const tabs = [
     { value: op.OUT_SERVICE_USAGE, label: 'Uso Serviço' },
@@ -43,41 +46,42 @@ export function TabSelector({
     handleReset();
   };
 
-  if (isDesktop) {
-    return (
-      <Tabs
-        value={movementTypeCode || tabs[0].value}
-        onValueChange={(value) =>
-          handleValueChange(value as MaterialOperationOutKey)
-        }
-        className='w-full'
-      >
-        <TabsList className='grid w-full grid-cols-5'>
-          {tabs.map((tab) => (
-            <TabsTrigger key={tab.value} value={tab.value}>
-              {tab.label}
-            </TabsTrigger>
-          ))}
-        </TabsList>
-      </Tabs>
-    );
-  }
-
   return (
-    <Select
-      value={movementTypeCode || tabs[0].value}
-      onValueChange={handleValueChange}
-    >
-      <SelectTrigger className='bg-primary/80 w-full text-gray-200'>
-        <SelectValue placeholder='Selecione um tipo de retirada' />
-      </SelectTrigger>
-      <SelectContent>
-        {tabs.map((tab) => (
-          <SelectItem key={tab.value} value={tab.value}>
-            {tab.label}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
+    <>
+      <div id='tab-selector-desktop' className='hidden md:block'>
+        <Tabs
+          value={movementTypeCode || tabs[0].value}
+          onValueChange={(value) =>
+            handleValueChange(value as MaterialOperationOutKey)
+          }
+          className='w-full'
+        >
+          <TabsList className='grid w-full grid-cols-5'>
+            {tabs.map((tab) => (
+              <TabsTrigger key={tab.value} value={tab.value}>
+                {tab.label}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+        </Tabs>
+      </div>
+      <div id='tab-selector-mobile' className='block md:hidden'>
+        <Select
+          value={movementTypeCode || tabs[0].value}
+          onValueChange={handleValueChange}
+        >
+          <SelectTrigger className='bg-primary/80 w-full text-gray-200'>
+            <SelectValue placeholder='Selecione um tipo de retirada' />
+          </SelectTrigger>
+          <SelectContent>
+            {tabs.map((tab) => (
+              <SelectItem key={tab.value} value={tab.value}>
+                {tab.label}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+    </>
   );
 }
--- apps/frontend/src/lib/is-mobile.ts
+++ apps/frontend/src/lib/is-mobile.ts
@@ -3,6 +3,9 @@ import { UAParser } from 'ua-parser-js';
 import { headers } from 'next/headers';
 
 export default async function isMobile(): Promise<boolean> {
+  // Uso de "dynamic function"
+  // Quando usa essa função a página não pode ser armazenada a priori, uma vez que apenas com a requisição do usuário será possível saber
+  // se o uso é através de mobile ou desktop
   const headersList = await headers();
   const userAgent = headersList.get('user-agent') || '';
 
